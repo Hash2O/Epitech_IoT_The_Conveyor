1191df58dfb252dc9544290e652a5bd724cac211
Hash2O <archeosam@gmail.com>
2026-02-26 14:57:48 +0100

    Minor changes in motor.cpp and motor.hpp

--- include/Motor.hpp
+++ include/Motor.hpp
@@ -2,24 +2,16 @@
 #define MOTOR_HPP
 
 #include <M5Stack.h>
-#include <AccelStepper.h>
-// #include <Wire.h>
-// #include "Module_GRBL_13.2.h"
+#include <Wire.h>
+#include "Module_GRBL_13.2.h"
 
 #define STARTING_MOTOR_AFTER_A_STOP "~"
 #define STOPPING_MOTOR "!"
 #define RELATIVE_MODE "G91"
 #define STEPMOTOR_I2C_ADDR 0x70
 
-#define STEP_PIN 26
-#define DIR_PIN 25
-
 class Motor
 {
-private:
-    AccelStepper stepper;
-    bool isMotorPaused;
-
 public:
     Motor();
     ~Motor() = default;
@@ -28,6 +20,10 @@ public:
     void handleMotorInstructions(bool isProductReferenced, bool shouldMotorBeActive);
     bool isPaused() const;
     void setMotorPaused(bool paused);
+
+private:
+    Module_GRBL grbl;
+    bool isMotorPaused;
 };
 
 #endif
--- src/Motor.cpp
+++ src/Motor.cpp
@@ -1,12 +1,53 @@
 
+// #include "Motor.hpp"
+
+// Motor::Motor() : stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN), isMotorPaused(true) {}
+
+// void Motor::initialize()
+// {
+//     stepper.setMaxSpeed(1000.0);
+//     stepper.setAcceleration(500.0);
+//     Serial.println("Motor initialized in absolute mode.");
+// }
+
+// void Motor::handleMotorInstructions(bool isProductReferenced, bool shouldMotorBeActive)
+// {
+//     if (isProductReferenced && shouldMotorBeActive) {
+//         if (isMotorPaused) {
+//             stepper.setSpeed(400.0);
+//             isMotorPaused = false;
+//         }
+//         stepper.runSpeed();
+//     } else {
+//         if (!isMotorPaused)
+//         {
+//         stepper.stop();
+//             isMotorPaused = true;
+//         }
+//         M5.Lcd.println("Motor is off");
+//     }
+// }
+
+// void Motor::setMotorPaused(bool paused)
+// {
+//     isMotorPaused = paused;
+// }
+
+// bool Motor::isPaused() const
+// {
+//     return isMotorPaused;
+// }
+
+
 #include "Motor.hpp"
 
-Motor::Motor() : stepper(AccelStepper::DRIVER, STEP_PIN, DIR_PIN), isMotorPaused(true) {}
+Motor::Motor() : grbl(STEPMOTOR_I2C_ADDR), isMotorPaused(true) {}
 
 void Motor::initialize()
 {
-    stepper.setMaxSpeed(1000.0);
-    stepper.setAcceleration(500.0);
+    Wire.begin(21, 22);
+    grbl.Init(&Wire);
+    grbl.setMode("absolute");
     Serial.println("Motor initialized in absolute mode.");
 }
 
@@ -14,14 +55,15 @@ void Motor::handleMotorInstructions(bool isProductReferenced, bool shouldMotorBe
 {
     if (isProductReferenced && shouldMotorBeActive) {
         if (isMotorPaused) {
-            stepper.setSpeed(400.0);
+            grbl.sendGcode(STARTING_MOTOR_AFTER_A_STOP);
             isMotorPaused = false;
         }
-        stepper.runSpeed();
+            grbl.sendGcode(RELATIVE_MODE);
+        grbl.setMotor(20, 20, 20, 200);
     } else {
         if (!isMotorPaused)
         {
-        stepper.stop();
+            grbl.sendGcode(STOPPING_MOTOR);
             isMotorPaused = true;
         }
         M5.Lcd.println("Motor is off");
